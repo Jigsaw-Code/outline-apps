15fecfecdc58159e9766ee9acbe0084eb216617d
alalamav <2132122+alalamav@users.noreply.github.com>
2018-05-23 13:39:22 -0400

    Windows: improve native error handling (#114)

--- electron/process_manager.ts
+++ electron/process_manager.ts
@@ -25,6 +25,10 @@ import * as url from 'url';
 import * as util from '../www/app/util';
 import * as errors from '../www/model/errors';
 
+import {SentryLogger} from './sentry_logger';
+
+const sentryLogger = new SentryLogger();
+
 // The returned path must be kept in sync with:
 //  - the destination path for the binaries in build_action.sh
 //  - the value specified for --config.asarUnpack in package_action.sh
@@ -67,28 +71,29 @@ export function launchProxy(
         throw errors.ErrorCode.SERVER_UNREACHABLE;
       })
       .then(() => {
-        return startLocalShadowsocksProxy(config, onDisconnected);
-      })
-      .catch((e) => {
-        throw errors.ErrorCode.SHADOWSOCKS_START_FAILURE;
-      })
-      .then(() => {
-        return validateServerCredentials();
-      })
-      .catch((e) => {
-        throw errors.ErrorCode.INVALID_SERVER_CREDENTIALS;
-      })
-      .then(() => {
-        return startHttpProxy();
-      })
-      .catch((e) => {
-        throw errors.ErrorCode.HTTP_PROXY_START_FAILURE;
-      })
-      .then((port) => {
-        configureSystemProxy(port);
-      })
-      .catch((e) => {
-        throw errors.ErrorCode.CONFIGURE_SYSTEM_PROXY_FAILURE;
+        return startLocalShadowsocksProxy(config, onDisconnected)
+            .catch((e) => {
+              throw errors.ErrorCode.SHADOWSOCKS_START_FAILURE;
+            })
+            .then(() => {
+              return validateServerCredentials()
+                  .catch((e) => {
+                    throw errors.ErrorCode.INVALID_SERVER_CREDENTIALS;
+                  })
+                  .then(() => {
+                    return startHttpProxy()
+                        .catch((e) => {
+                          throw errors.ErrorCode.HTTP_PROXY_START_FAILURE;
+                        })
+                        .then((port) => {
+                          try {
+                            configureSystemProxy(port);
+                          } catch (e) {
+                            throw errors.ErrorCode.CONFIGURE_SYSTEM_PROXY_FAILURE;
+                          }
+                        });
+                  });
+            });
       });
 }
 
@@ -129,12 +134,12 @@ function startLocalShadowsocksProxy(
       ssLocal.on('exit', (code, signal) => {
         // We assume any signal sent to ss-local was sent by us.
         if (signal) {
-          console.log(`ss-local exited with signal ${signal}`);
+          sentryLogger.info(`ss-local exited with signal ${signal}`);
           onDisconnected();
           return;
         }
 
-        console.log(`ss-local exited with code ${code}`);
+        sentryLogger.info(`ss-local exited with code ${code}`);
         onDisconnected();
       });
 
@@ -222,10 +227,10 @@ function stopHttpProxy() {
       // and log when it finally happens.
       httpProxy.close((e: Error) => {
         if (e) {
-          console.error('could not stop HTTP proxy', e);
+          sentryLogger.error(`could not stop HTTP proxy: ${e}`);
           return;
         }
-        console.log('HTTP proxy stopped');
+        sentryLogger.info('HTTP proxy stopped');
       });
     }
     resolve();
@@ -246,7 +251,7 @@ function configureSystemProxy(httpProxyPort: number) {
 
 // Configures the system to no longer use our proxy.
 function resetSystemProxy() {
-  console.log(`resetting system proxy`);
+  sentryLogger.info(`resetting system proxy`);
   try {
     execFileSync(pathToEmbeddedExe('setsystemproxy'), ['off']);
   } catch (e) {
