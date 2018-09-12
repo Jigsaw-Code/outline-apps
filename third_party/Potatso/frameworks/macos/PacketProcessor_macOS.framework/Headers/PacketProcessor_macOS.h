4fe9158acdb0c171145de4834f5a334e526dd0c3
alalamav <alalamav@users.noreply.github.com>
2018-09-12 16:03:38 -0400

    Update PacketProcessor framework

---
+++ third_party/Potatso/frameworks/macos/PacketProcessor_macOS.framework/Headers/PacketProcessor_macOS.h
@@ -0,0 +1,25 @@
+// Copyright 2017 The Outline Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Tun2Socks_macOS.
+FOUNDATION_EXPORT double PacketProcessor_macOSVersionNumber;
+
+//! Project version string for Tun2Socks_macOS.
+FOUNDATION_EXPORT const unsigned char PacketProcessor_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PacketProcessor_macOS/PublicHeader.h>
+
+#import "TunnelInterface.h"
