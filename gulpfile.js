147944aee9758d4a346740ed1232e9e71669c57a
littleguga <fed777os@gmail.com>
2018-10-25 05:54:42 +0300

    add check if system is running Windows, if so - add path for sh in scripts

--- gulpfile.js
+++ gulpfile.js
@@ -148,7 +148,9 @@ function writeEnvJson(platform, config, isRelease) {
     throw new Error(`Failed to set up environment, no such path: ${config.targetDir}`);
   }
   const envFile = `${config.targetDir}/environment.json`;
-  runCommand(`scripts/environment_json.sh -p ${platform} ${isRelease ? '-r' : ''} > ${envFile}`, {}, function () {
+  const bashPathForWindows = process.platform.includes('win') ? 'sh ' : '';
+  const envScript = `${bashPathForWindows}scripts/environment_json.sh`;
+  runCommand(`${envScript} -p ${platform} ${isRelease ? '-r' : ''} > ${envFile}`, {}, function () {
     gutil.log(`Wrote ${envFile}`);
   });
 }
