6bfc5e70aedc7dda330bc378720a9f3632604b5c
Trevor Johnston <trevj@users.noreply.github.com>
2018-12-13 11:40:46 -0500

    fix runtime error on windows

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -25,8 +25,6 @@ const LINUX_DAEMON_FILENAME = 'OutlineProxyController';
 const LINUX_DAEMON_SYSTEMD_SERVICE_FILENAME = 'outline_proxy_controller.service';
 const LINUX_INSTALLER_FILENAME = 'install_linux_service.sh';
 
-const LINUX_TEMP_FOLDER = fs.mkdtempSync('/tmp/');
-
 export function pathToEmbeddedBinaryFolder() {
   return path.join(__dirname.replace('app.asar', 'app.asar.unpacked'), 'bin', os.platform());
 }
@@ -50,38 +48,23 @@ export function getServiceStartCommand(): string {
             app.getAppPath().includes('app.asar') ? path.dirname(app.getPath('exe')) :
                                                     app.getAppPath(),
             'install_' + os.platform() + '_service.' + (isWindows ? 'bat' : 'sh'))}"`;
-
   } else if (isLinux) {
-    copyServiceFilesToTempFolder();
-
-    return path.join(LINUX_TEMP_FOLDER, LINUX_INSTALLER_FILENAME);
+    return path.join(copyServiceFilesToTempFolder(), LINUX_INSTALLER_FILENAME);
   } else {
     throw new Error('Unsupported Operating System');
   }
 }
 
-// In some linux distro (tested on Debian) root is  disallowed to access the appImage mounted folder
-// so we need to copy these files out of there so root can copy them in privilage folders
+// On some distributions, root is not allowed access the AppImage folder.
+// Instead, copy those files to /tmp.
 export function copyServiceFilesToTempFolder() {
-  // create a tmp folder which root can access
-  try {
-    fs.mkdirSync(LINUX_TEMP_FOLDER);
-  } catch (err) {
-    if (err.code !== 'EEXIST') throw err;
-  }
-
-  const binDestination = pathToEmbeddedBinaryFolder();
-
-  const serviceRelatedFiles =
-      [LINUX_DAEMON_FILENAME, LINUX_DAEMON_SYSTEMD_SERVICE_FILENAME, LINUX_INSTALLER_FILENAME];
-  serviceRelatedFiles.forEach((currentFile) => {
-    const sourceDaemonFile = pathToEmbeddedBinary(currentFile);
-    const destDaemonFile = path.join(LINUX_TEMP_FOLDER, currentFile);
-
-    try {
-      fsextra.copySync(sourceDaemonFile, destDaemonFile, {overwrite: true});
-    } catch (err) {
-      throw err;
-    }
-  });
+  const tmp = fs.mkdtempSync('/tmp/');
+  [LINUX_DAEMON_FILENAME, LINUX_DAEMON_SYSTEMD_SERVICE_FILENAME, LINUX_INSTALLER_FILENAME].forEach(
+      (filename) => {
+        const src = pathToEmbeddedBinary(filename);
+        // https://github.com/jprichardson/node-fs-extra/issues/323
+        const dest = path.join(tmp, filename);
+        fsextra.copySync(src, dest, {overwrite: true});
+      });
+  return tmp;
 }
