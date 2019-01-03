c36d298f587c9e7dd34d6b15eb45ff17edcbbf8a
trevj <trevj@users.noreply.github.com>
2019-01-03 17:33:43 -0500

    move to typescript 3.1 (#472)

--- src/www/app/updater.ts
+++ src/www/app/updater.ts
@@ -20,7 +20,7 @@ export interface Updater {
 }
 
 export class AbstractUpdater implements Updater {
-  private listener: UpdateListener;
+  private listener: UpdateListener|null = null;
 
   setListener(listener: UpdateListener) {
     this.listener = listener;
