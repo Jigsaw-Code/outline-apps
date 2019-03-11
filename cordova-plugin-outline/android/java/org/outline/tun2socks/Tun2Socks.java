ac06a38583393d85063c2797c428ac78658b2c9c
alalamav <alalamav@users.noreply.github.com>
2019-03-11 18:18:52 -0400

    Perform packet I/O in Go

--- cordova-plugin-outline/android/java/org/outline/tun2socks/Tun2Socks.java
+++ cordova-plugin-outline/android/java/org/outline/tun2socks/Tun2Socks.java
@@ -1,12 +1,6 @@
 package org.outline.tun2socks;
 
 import android.os.ParcelFileDescriptor;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.Locale;
 import java.util.logging.Logger;
 
 /**
@@ -15,10 +9,6 @@ import java.util.logging.Logger;
 public class Tun2Socks {
   private static final String LOG_TAG = "Tun2Socks";
   private static final Logger LOG = Logger.getLogger(LOG_TAG);
-  private static final int MTU = 1500;
-
-  private static PacketFlow packetFlow;
-  private static volatile boolean isRunning = false;
 
   /**
    * Starts directing traffic through tun2socks. Blocks until `Tun2Socks.stop` is called.
@@ -34,75 +24,13 @@ public class Tun2Socks {
     if (tunFd == null || socksServerAddress == null || socksServerPort <= 0) {
       throw new IllegalArgumentException("Must provide a TUN file descriptor, proxy host and port");
     }
-    final FileDescriptor fd = tunFd.getFileDescriptor();
-    packetFlow = new PacketFlow(new FileOutputStream(fd));
-    tun2socks.Tun2socks.startSocks(packetFlow, socksServerAddress, socksServerPort);
-    isRunning = true;
-    readPackets(new FileInputStream(fd));
+    tun2socks.Tun2socks.startSocks(tunFd.getFd(), socksServerAddress, socksServerPort);
   }
 
   /**
    * Stops directing traffic through tun2socks.
    */
   public static void stop() {
-    isRunning = false;
     tun2socks.Tun2socks.stopSocks();
-    packetFlow.close();
-  }
-
-  /**
-   * Reads packets from `inputStream` and writes them to tun2socks.
-   */
-  private static void readPackets(FileInputStream inputStream) {
-    ByteBuffer buffer = ByteBuffer.allocate(MTU);
-    while (isRunning) {
-      try {
-        int readBytes = inputStream.read(buffer.array());
-        if (readBytes <= 0) {
-          LOG.warning(String.format(Locale.ROOT, "Read %d bytes from TUN", readBytes));
-          continue;
-        }
-        buffer.limit(readBytes);
-        tun2socks.Tun2socks.inputPacket(buffer.array());
-        buffer.clear();
-      } catch (IOException e) {
-        LOG.severe(
-            String.format(Locale.ROOT, "Failed to read packet from TUN: %s", e.getMessage()));
-      }
-    }
-    try {
-      inputStream.close();
-    } catch (IOException e) {
-      LOG.warning("Failed to close TUN input stream");
-    }
-  }
-
-  /**
-   * Implementation of the `tun2socks.PacketFlow` interface. tun2socks calls this class'
-   * `writePacket` method to send data to the VPN.
-   */
-  private static class PacketFlow implements tun2socks.PacketFlow {
-    private FileOutputStream outputStream;
-
-    PacketFlow(FileOutputStream outputStream) {
-      this.outputStream = outputStream;
-    }
-
-    @Override
-    public void writePacket(byte[] packet) {
-      try {
-        outputStream.write(packet);
-      } catch (IOException e) {
-        LOG.severe(String.format(Locale.ROOT, "Failed to write packet to TUN: %s", e.getMessage()));
-      }
-    }
-
-    public void close() {
-      try {
-        outputStream.close();
-      } catch (IOException e) {
-        LOG.warning("Failed to close TUN output stream");
-      }
-    }
   }
 }
