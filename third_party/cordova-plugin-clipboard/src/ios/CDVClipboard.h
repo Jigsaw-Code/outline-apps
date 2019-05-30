dd5ba7004d19d4f6f3d73f0a906dd8a01f2d90eb
alalamav <alalamav@users.noreply.github.com>
2019-05-30 14:29:14 -0400

    Revert to using plugins hosted on GitHub

--- third_party/cordova-plugin-clipboard/src/ios/CDVClipboard.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Cordova/CDVPlugin.h>
-
-@interface CDVClipboard : CDVPlugin {}
-
-- (void)copy:(CDVInvokedUrlCommand*)command;
-- (void)paste:(CDVInvokedUrlCommand*)command;
-
-@end
