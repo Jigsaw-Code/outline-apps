910084fa9749e7cc33ddcfa2f71fa2b72fd63414
alalamav <alalamav@users.noreply.github.com>
2020-09-09 21:43:14 -0400

    Version control cordova-osx files as a workaround for a failure to restore plugins

---
+++ apple/xcode/osx/www/cordova_plugins.js
@@ -0,0 +1,44 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-outline.OutlinePlugin",
+      "file": "plugins/cordova-plugin-outline/outlinePlugin.js",
+      "pluginId": "cordova-plugin-outline",
+      "clobbers": [
+        "cordova.plugins.outline"
+      ]
+    },
+    {
+      "id": "cordova-plugin-clipboard.Clipboard",
+      "file": "plugins/cordova-plugin-clipboard/www/clipboard.js",
+      "pluginId": "cordova-plugin-clipboard",
+      "clobbers": [
+        "cordova.plugins.clipboard"
+      ]
+    },
+    {
+      "id": "cordova-webintent.WebIntent",
+      "file": "plugins/cordova-webintent/www/webintent.js",
+      "pluginId": "cordova-webintent",
+      "clobbers": [
+        "WebIntent"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-custom-config": "5.1.0",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-outline": "0.0.0",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-clipboard": "2.0.0",
+    "cordova-webintent": "2.0.0"
+  };
+});
