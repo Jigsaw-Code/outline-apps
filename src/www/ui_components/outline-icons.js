77543ac280a6eba9747bd08cf25d64bf8407882d
Martin McRoy <mpmcroy@google.com>
2021-01-21 11:18:12 -0500

    Addresses review comments

--- src/www/ui_components/outline-icons.js
+++ src/www/ui_components/outline-icons.js
@@ -1,17 +1,3 @@
-const $_documentContainer = document.createElement('template');
-
-$_documentContainer.innerHTML = `<iron-iconset-svg name="outline-icons" size="24">
-  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
-    <g id="outline">
-      <path d="M23.9,10.7c-0.6-5.4-4.8-9.6-10.1-10.2v4.7c3.7,0.7,6.1,4.2,5.5,7.9c-0.5,2.8-2.7,5.1-5.5,5.6v4.7
-        C20.1,22.6,24.6,17,23.9,10.7z"></path>
-      <path d="M0.1,13.1c0.6,5.4,4.8,9.6,10.1,10.2v-4v-0.6V5.2V3.9V0.5C4,1.3-0.5,6.9,0.1,13.1z"></path>
-    </g>
-  </svg>
-</iron-iconset-svg>`;
-
-document.head.appendChild($_documentContainer.content);
-
 /*
   Copyright 2020 The Outline Authors
 
@@ -27,4 +13,18 @@ document.head.appendChild($_documentContainer.content);
   See the License for the specific language governing permissions and
   limitations under the License.
 */
+
+const $_documentContainer = document.createElement('template');
+
+$_documentContainer.innerHTML = `<iron-iconset-svg name="outline-icons" size="24">
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+    <g id="outline">
+      <path d="M23.9,10.7c-0.6-5.4-4.8-9.6-10.1-10.2v4.7c3.7,0.7,6.1,4.2,5.5,7.9c-0.5,2.8-2.7,5.1-5.5,5.6v4.7
+        C20.1,22.6,24.6,17,23.9,10.7z"></path>
+      <path d="M0.1,13.1c0.6,5.4,4.8,9.6,10.1,10.2v-4v-0.6V5.2V3.9V0.5C4,1.3-0.5,6.9,0.1,13.1z"></path>
+    </g>
+  </svg>
+</iron-iconset-svg>`;
+
+document.head.appendChild($_documentContainer.content);
 ;
