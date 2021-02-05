98248fa286e9daa9d5fefb82c0ee0b50c9c4dccb
Martin McRoy <mpmcroy@google.com>
2021-02-05 09:59:52 -0500

    Auto-format

--- src/www/model/events.spec.ts
+++ src/www/model/events.spec.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {EventQueue, ServerAdded, ServerConnected, ServerForgotten, ServerRenamed} from "./events";
+import {EventQueue, ServerAdded, ServerConnected, ServerForgotten, ServerRenamed} from './events';
 
 describe('EventQueue', () => {
   it('subscribe registers listeners to corresponding event', () => {
