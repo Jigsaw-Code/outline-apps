c32f36db7066b475406effb2ff8f58d8af2c2a9f
Daniel LaCosse <d@niellacos.se>
2021-11-05 11:11:06 -0400

    update run_action

--- scripts/run_action.sh
+++ scripts/run_action.sh
@@ -14,18 +14,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eux
+set -eu
 
 export ROOT_DIR="${ROOT_DIR:-$(git rev-parse --show-toplevel)}"
 export BUILD_DIR="${BUILD_DIR:-$ROOT_DIR/build}"
+export RUN_ACTION_INDENT=''
 
 function run_action() {
-  local action=$1
-  echo "[Running $action]"
+  local -r ACTION="${1:-""}"
+  local -r STYLE_BOLD_WHITE='\033[1;37m'
+  local -r STYLE_BOLD_GREEN='\033[1;32m'
+  local -r STYLE_BOLD_RED='\033[1;31m'
+  local -r STYLE_RESET='\033[0m'
+  local -r OLD_INDENT="${RUN_ACTION_INDENT}"
+
+  RUN_ACTION_INDENT="=> ${RUN_ACTION_INDENT}"
+
+  if [[ "${ACTION}" == "" ]]; then
+    echo -e "Please provide an action to run. ${STYLE_BOLD_WHITE}List of valid actions:${STYLE_RESET}\n"
+    find . -name '*.action.sh' | sed -E 's:./(.*).action.sh:\1:'
+    exit 0
+  fi
+
+  echo -e "${OLD_INDENT}${STYLE_BOLD_WHITE}[Running ${ACTION}]${STYLE_RESET}"
   shift
-  "$ROOT_DIR/${action}.action.sh" "$@"
-  echo "[Done $action]"
+
+  "${ROOT_DIR}/${ACTION}.action.sh" "$@"
+
+  local -ir STATUS="$?"
+  if [[ "${STATUS}" == "0" ]]; then
+    echo -e "${OLD_INDENT}${STYLE_BOLD_GREEN}[${ACTION}: Finished]${STYLE_RESET}"
+  else
+    echo -e "${OLD_INDENT}${STYLE_BOLD_RED}[${ACTION}: Failed]${STYLE_RESET}"
+  fi
+
+  RUN_ACTION_INDENT="${OLD_INDENT}"
+
+  return "${STATUS}"
 }
+
 export -f run_action
 
 run_action "$@"
