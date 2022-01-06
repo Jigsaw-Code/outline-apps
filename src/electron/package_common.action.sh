9c16846f363b79e7a8f415b0b86bb92cd15115ab
Daniel LaCosse <d@niellacos.se>
2022-01-06 13:13:11 -0500

    version defaults

--- src/electron/package_common.action.sh
+++ src/electron/package_common.action.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION=$1
+VERSION=${1:-0.0.0}
 
 npm run action src/electron/build $VERSION
 
