265559087f32f5688f16075c912e8c8c10529c46
Daniel LaCosse <d@niellacos.se>
2022-01-20 16:45:58 -0500

    default

--- src/electron/package_linux.action.sh
+++ src/electron/package_linux.action.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 PLATFORM=
-BUILD_MODE=
+BUILD_MODE=debug
 for i in "$@"; do
     case $i in
     --platform=*)
