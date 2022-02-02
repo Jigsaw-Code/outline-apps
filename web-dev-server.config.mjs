7b3306377ee318e7e8b68a8d742e2415da318365
Daniel LaCosse <d@niellacos.se>
2022-02-02 17:03:06 -0500

    small cleanup

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -4,12 +4,15 @@ import {storybookPlugin} from "@web/dev-server-storybook";
 export default {
   nodeResolve: true,
   open: true,
-  plugins: [esbuildPlugin({
-    ts: true,
-    target: "auto"
-  }), storybookPlugin({
-    type: "web-components"
-  })],
+  plugins: [
+    esbuildPlugin({
+      ts: true,
+      target: "auto"
+    }),
+    storybookPlugin({
+      type: "web-components"
+    })
+  ],
   rootDir: "./src/www",
   watch: true
 };
