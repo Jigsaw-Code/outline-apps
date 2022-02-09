b04bc9c0c6ccc61540e7e8edc1bf336b8cdc6dfb
Daniel LaCosse <d@niellacos.se>
2022-02-09 12:45:05 -0500

    ah yes, race

--- scripts/get_version.mjs
+++ scripts/get_version.mjs
@@ -43,7 +43,7 @@ export async function getVersion(platform) {
     case "windows":
       return "1.7.0";
     case "linux":
-      return "1.7.0";
+      return "1.7.1";
     default:
       throw new Error("get_version must be provided a platform argument");
   }
