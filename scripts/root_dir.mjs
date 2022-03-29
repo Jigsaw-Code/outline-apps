48e2b88433de5c2f55f554c097b5c603e0e6a3aa
Daniel LaCosse <daniellacosse@google.com>
2022-03-28 22:17:57 -0400

    ah

--- scripts/root_dir.mjs
+++ scripts/root_dir.mjs
@@ -18,12 +18,12 @@ import url from "url";
 // WARNING: if you move this file, you MUST update this file path
 const RELATIVE_PATH_FROM_THIS_FILE_TO_PROJECT_ROOT = "../..";
 
-export function rootDir(fromLocation) {
+export function rootDir(fromLocation = process.argv[1]) {
   return path.resolve(fromLocation, RELATIVE_PATH_FROM_THIS_FILE_TO_PROJECT_ROOT);
 }
 
 async function main() {
-  console.log(rootDir(process.argv[1]));
+  console.log(rootDir());
 }
 
 if (import.meta.url === url.pathToFileURL(process.argv[1]).href) {
