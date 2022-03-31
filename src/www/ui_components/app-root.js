6f6fa70bfa4df815c5f866eadc8ab84fe0075e46
Daniel LaCosse <daniellacosse@google.com>
2022-03-31 17:41:00 -0400

    more

--- src/www/ui_components/app-root.js
+++ src/www/ui_components/app-root.js
@@ -44,7 +44,7 @@ import "./language-view.js";
 import "./licenses-view.js";
 import "./outline-icons.js";
 import "./privacy-view.js";
-import "./server_list.ts";
+import "./server_list";
 import "./server_card";
 import "./server_card/server_connection_viz";
 import "./servers-view.js";
