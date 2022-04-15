652f3fc63a6404c23e76fdcfe14b7401b4434733
Vinicius Fortuna <fortuna@users.noreply.github.com>
2022-04-15 15:42:04 -0400

    Project diff

--- src/cordova/apple/xcode/osx/www/cordova_plugins.js
+++ src/cordova/apple/xcode/osx/www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
-      "id": "cordova-plugin-clipboard.Clipboard",
-      "file": "plugins/cordova-plugin-clipboard/www/clipboard.js",
-      "pluginId": "cordova-plugin-clipboard",
-      "clobbers": [
-        "cordova.plugins.clipboard"
-      ]
-    },
-    {
       "id": "cordova-webintent.WebIntent",
       "file": "plugins/cordova-webintent/www/webintent.js",
       "pluginId": "cordova-webintent",
@@ -18,8 +10,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-outline": "0.0.0",
-    "cordova-plugin-clipboard": "2.0.0",
     "cordova-webintent": "2.0.0"
   };
 });
