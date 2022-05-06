ca818368b29c58ee0e6ba2948056fd16ba80d10f
Daniel LaCosse <daniellacosse@google.com>
2022-05-06 17:04:11 -0400

    damit lol

--- commitlint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [2, 'always', ['android', 'apple', 'cordova', 'devtools', 'electron', 'linux', 'windows', 'www']],
-  },
-};
