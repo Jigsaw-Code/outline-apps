bd9fc092e06f941b7520ccc3e0ce10bd867e3d83
Daniel LaCosse <daniellacosse@google.com>
2022-05-09 09:39:14 -0400

    add scopes, comment, permissions

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['android', 'apple', 'cordova', 'devtools', 'electron', 'linux', 'windows', 'www']],
+    'scope-enum': [
+      2,
+      'always',
+      ['android', 'apple', 'ios', 'macos', 'cordova', 'devtools', 'electron', 'linux', 'windows', 'www'],
+    ],
   },
 };
