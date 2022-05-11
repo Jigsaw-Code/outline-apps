381bb65f951c342353b0cc1dee918f2afb77f29a
Daniel LaCosse <daniellacosse@google.com>
2022-05-11 15:16:09 -0400

    rewrite timeoutpromise

--- src/www/app/util.ts
+++ src/www/app/util.ts
@@ -14,20 +14,11 @@
 
 import * as errors from '../model/errors';
 
-export function timeoutPromise<T>(promise: Promise<T>, ms: number, name = ''): Promise<T | undefined> {
-  let winner: Promise<T | undefined> | undefined = undefined;
-  const timeout = new Promise<T>((resolve, reject) => {
-    const timeoutId = setTimeout(() => {
-      clearTimeout(timeoutId);
-      if (winner !== undefined) {
-        console.log(`Promise "${name}" resolved before ${ms} ms.`);
-        resolve(undefined);
-      } else {
-        console.log(`Promise "${name}" timed out after ${ms} ms.`);
-        reject(new errors.OperationTimedOut(ms, name));
-      }
-    }, ms);
-  });
-  winner = Promise.race([promise, timeout]);
-  return winner;
+export function timeoutPromise<T>(promise: Promise<T>, timeoutDuration: number, timeoutName: string): Promise<T> {
+  return Promise.race([
+    promise,
+    new Promise<T>((_, reject) =>
+      setTimeout(() => reject(new errors.OperationTimedOut(timeoutDuration, timeoutName)), timeoutDuration)
+    ),
+  ]);
 }
