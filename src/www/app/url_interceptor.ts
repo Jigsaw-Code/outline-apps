a5316e37c93f66c287eef27c94c04f3de83e318e
Daniel LaCosse <daniellacosse@google.com>
2022-05-11 14:33:50 -0400

    nix triple slash

--- src/www/app/url_interceptor.ts
+++ src/www/app/url_interceptor.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import '../../types/ambient/webintents.d.ts';
+/// <reference path='../../types/ambient/webintents.d.ts'/>
 
 export class UrlInterceptor {
   protected launchUrl?: string;
