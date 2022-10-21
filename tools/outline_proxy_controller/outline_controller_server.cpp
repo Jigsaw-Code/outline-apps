aaa481fcd66dd41eab92523f34aabce384733dd6
jyyi1 <yijunyi@google.com>
2022-10-21 15:20:12 -0400

    feat(service/linux): ✨ add network monitoring logic

--- tools/outline_proxy_controller/outline_controller_server.cpp
+++ tools/outline_proxy_controller/outline_controller_server.cpp
@@ -22,22 +22,30 @@
 #include <pwd.h>
 
 #include <boost/asio.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
+#include <boost/json.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 
 #include "logger.h"
+#include "network_monitor.h"
 #include "outline_controller_server.h"
-#include "outline_error.h"
 
 using namespace outline;
 
 //#region OutlineClientSession Implementation
 
-// Routing commands from App
+// Routing commands from App (src/electron/routing_service.ts)
 static const std::string kConfigureRoutingAction = "configureRouting";
 static const std::string kResetRoutingAction = "resetRouting";
-static const std::string kGetDeviceNameAction = "getDeviceName";
+static const std::string kNotifyStatusChangedAction = "statusChanged";
+
+// Response fields (src/electron/routing_service.ts)
+static const char* const kResponseActionField = "action";
+static const char* const kResponseStatusField = "statusCode";
+static const char* const kResponseErrorField = "errorMessage";
+static const char* const kResponseConnectionStateField = "connectionStatus";
 
 // Minimum length of JSON input from app
 static const int kJsonInputMinLength = 10;
@@ -68,8 +76,8 @@ static bool TryParseJson(const std::string &raw_str, boost::property_tree::ptree
 OutlineClientSession::OutlineClientSession(
   boost::asio::local::stream_protocol::socket &&channel,
   std::shared_ptr<OutlineProxyController> outline_proxy_controller)
-  : channel_(std::move(channel)),
-    outline_controller_(outline_proxy_controller)
+  : channel_{std::move(channel)},
+    outline_controller_{outline_proxy_controller}
 {
   logger.info("client session started");
 }
@@ -79,12 +87,18 @@ OutlineClientSession::~OutlineClientSession() {
 }
 
 boost::asio::awaitable<void> OutlineClientSession::Start() {
+  using namespace boost::asio::experimental::awaitable_operators;
+
+  // Wait any one to success (and cancels the other), ignoring errors
+  co_await (ServeClientCommands() || MonitorNetworkChanges());
+}
+
+boost::asio::awaitable<void> OutlineClientSession::ServeClientCommands() {
   using namespace boost::asio;
 
   try {
     std::string client_command, raw_buffer;
     boost::property_tree::ptree request_obj;
-    std::ostringstream response;
     for (;;) {
       do {
         co_await async_read_until(
@@ -95,20 +109,58 @@ boost::asio::awaitable<void> OutlineClientSession::Start() {
 
       logger.debug("handling client request \"" + client_command + "\"...");
       auto result = RunClientCommand(request_obj);
-
-      // TODO: replace the following code with a json library to handle special characters
-      response << "{\"statusCode\": " << result.status
-               << ",\"returnValue\": \"" << result.result << "\""
-               << ",\"action\": \"" << result.action << "\"}";
-      co_await async_write(channel_, buffer(response.str()), use_awaitable);
-      logger.debug("Wrote back \"" + response.str() + "\" to unix socket");
+      co_await SendResponse(result);
 
       client_command.clear();
-      response.str(std::string{});
     }
   } catch (const std::exception& e) {
     channel_.close();
   }
+}
+
+boost::asio::awaitable<void> OutlineClientSession::MonitorNetworkChanges() {
+  using namespace boost::asio;
+
+  auto executor = co_await this_coro::executor;
+  try {
+    NetworkMonitor net_monitor{executor};
+    for (;;) {
+      // No need to detect the race condition of whether the change is caused by
+      // us (which might cause infinite loop) because we are running in a single-thread
+      // coroutine (see OutlineClientSession::Start()), the code after co_await will be
+      // executed either before RunClientCommand() or after RunClientCommand().
+      auto evt = co_await net_monitor.WaitForChangeEvent();
+      if (evt.HasRoutingChanged()) {
+        if (outline_controller_->IsOutlineRoutingPolluted()) {
+          logger.debug("network condition changed, reconnecting...");
+          co_await SendResponse(ConnectionStateChangedResult(ConnectionState::kReconnecting));
+          if (outline_controller_->TryReconfigureRouting()) {
+            logger.info("reconnected successfully");
+            co_await SendResponse(ConnectionStateChangedResult(ConnectionState::kConnected));
+          }
+        }
+      }
+    }
+  } catch (const std::exception &err) {
+    logger.warn("failed to monitor network changes due to:");
+    logger.warn(err.what());
+    logger.warn("will ignore further network changes");
+    throw;
+  }
+}
+
+OutlineClientSession::CommandResult OutlineClientSession::SucceededResult(const std::string &action) {
+  return {static_cast<int>(ErrorCode::kOk), std::nullopt, action, std::nullopt};
+}
+
+OutlineClientSession::CommandResult OutlineClientSession::ErrorResult(
+  ErrorCode code, const std::string &err_msg, const std::string &action)
+{
+  return {static_cast<int>(code), err_msg, action, std::nullopt};
+}
+
+OutlineClientSession::CommandResult OutlineClientSession::ConnectionStateChangedResult(ConnectionState state) {
+  return {static_cast<int>(ErrorCode::kOk), std::nullopt, kNotifyStatusChangedAction, state};
 }
 
 OutlineClientSession::CommandResult OutlineClientSession::RunClientCommand(const boost::property_tree::ptree &request) {
@@ -117,7 +169,7 @@ OutlineClientSession::CommandResult OutlineClientSession::RunClientCommand(const
   auto action_iter = request.find("action");
   if (action_iter == request.not_found()) {
     logger.error("Invalid input JSON - action doesn't exist");
-    return {static_cast<int>(ErrorCode::kUnexpected), "Invalid JSON", {}};
+    return ErrorResult(ErrorCode::kUnexpected, "Invalid JSON", {});
   }
 
   action = boost::lexical_cast<std::string>(request.to_iterator(action_iter)->second.data());
@@ -128,38 +180,56 @@ OutlineClientSession::CommandResult OutlineClientSession::RunClientCommand(const
       auto parameters_iter = request.find("parameters");
       if (parameters_iter == request.not_found()) {
         logger.error("Invalid input JSON - parameters doesn't exist");
-        return {static_cast<int>(ErrorCode::kUnexpected), "Invalid JSON", action};
+        return ErrorResult(ErrorCode::kUnexpected, "Invalid JSON", action);
       }
       const auto parameters = request.to_iterator(parameters_iter)->second;
       auto proxyIp_iter = parameters.find("proxyIp");
       if (proxyIp_iter == parameters.not_found()) {
         logger.error("Invalid input JSON - parameters doesn't exist");
-        return {static_cast<int>(ErrorCode::kUnexpected), "Invalid JSON", action};
+        return ErrorResult(ErrorCode::kUnexpected, "Invalid JSON", action);
       }
       outline_server_ip =
           boost::lexical_cast<std::string>(request.to_iterator(proxyIp_iter)->second.data());
       outline_controller_->routeThroughOutline(outline_server_ip);
       logger.info("Configure Routing to " + outline_server_ip + " is done.");
-      return {static_cast<int>(ErrorCode::kOk), {}, action};
+      return SucceededResult(action);
     } else if (action == kResetRoutingAction) {
       outline_controller_->routeDirectly();
       logger.info("Reset Routing done");
-      return {static_cast<int>(ErrorCode::kOk), {}, action};
-    } else if (action == kGetDeviceNameAction) {
-      logger.info("Get device name done");
-      return {static_cast<int>(ErrorCode::kOk), outline_controller_->getTunDeviceName(), action};
+      return SucceededResult(action);
     } else {
       logger.error("Invalid action specified in JSON (" + action + ")");
-      return {static_cast<int>(ErrorCode::kUnexpected), "Undefined Action", {}};
+      return ErrorResult(ErrorCode::kUnexpected, "Undefined Action", {});
     }
   } catch (const std::system_error& err) {
     logger.error("[" + err.code().message() + "] " + err.what());
     if (err.code().category() == OutlineErrorCategory()) {
       // TODO: add err.what() to give more details to the client
-      return {err.code().value(), {}, action};
+      return ErrorResult(static_cast<ErrorCode>(err.code().value()), err.what(), action);
     }
-    return {static_cast<int>(ErrorCode::kUnexpected), {}, action};
+    return ErrorResult(ErrorCode::kUnexpected, err.what(), action);
   }
+}
+
+boost::asio::awaitable<void> OutlineClientSession::SendResponse(const OutlineClientSession::CommandResult &response) {
+  using namespace boost::asio;
+  using namespace boost::json;
+
+  // TODO: prevent any race conditions by introducing an output queue
+  object json_output{
+    { kResponseStatusField, response.status_code },
+    { kResponseActionField, response.action }
+  };
+  if (response.error_message) {
+    json_output[kResponseErrorField] = *response.error_message;
+  }
+  if (response.connection_state) {
+    json_output[kResponseConnectionStateField] = static_cast<int>(*response.connection_state);
+  }
+
+  auto body{serialize(json_output)};
+  co_await async_write(channel_, buffer(body), use_awaitable);
+  logger.debug("Wrote back " + body + " to unix socket");
 }
 
 //#endregion OutlineClientSession Implementation
