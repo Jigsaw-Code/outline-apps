54ff9844a6fe421a38ff0d47648c9cec32f4256f
Ben Schwartz <bemasc@google.com>
2022-11-04 15:41:45 -0400

    Add nil prefix check

--- cordova-plugin-outline/apple/vpn/PacketTunnelProvider.m
+++ cordova-plugin-outline/apple/vpn/PacketTunnelProvider.m
@@ -274,14 +274,15 @@ static NSDictionary *kVpnSubnetCandidates;  // Subnets to bind the VPN.
   config.port = [self.tunnelConfig.port intValue];
   config.password = self.tunnelConfig.password;
   config.cipherName = self.tunnelConfig.method;
-
-  uint8_t prefixBytes[self.tunnelConfig.prefix.length];
-  for (int i = 0; i < self.tunnelConfig.prefix.length; i++) {
-    prefixBytes[i] = [self.tunnelConfig.prefix characterAtIndex:i];
+  if (self.tunnelConfig.prefix != nil) {
+    NSString* prefixStr = self.tunnelConfig.prefix;
+    uint8_t prefixBytes[prefixStr.length];
+    for (int i = 0; i < prefixStr.length; i++) {
+      prefixBytes[i] = [prefixStr characterAtIndex:i];
+    }
+    config.prefix = [[NSData alloc] initWithBytes:prefixBytes
+                                           length:prefixStr.length];
   }
-  config.prefix = [[NSData alloc] initWithBytes:prefixBytes
-                                         length:self.tunnelConfig.prefix.length];
-
   NSError *err;
   ShadowsocksClient* client = ShadowsocksNewClient(config, &err);
   if (err != nil) {
