76b1b863576aafa9ee7840d4cea6ceaf097030a4
Daniel LaCosse <d@niellacos.se>
2022-11-23 11:00:51 -0500

    chore(cordova): moving cordova plugin into the src folder (#1467)
    * testing to see if this breaks anything
    
    * ran npm install
    
    * add cordova-plugin-outline to workspace
    
    * okay fine, I'll do this
    
    * move it again
    
    * update path to plugin for android message scripts
    
    * src/cordova_plugin
    
    * rename to cordova/plugin
    
    * reset third_party (oops)

--- src/cordova/plugin/android/scripts/copy_third_party.js
+++ src/cordova/plugin/android/scripts/copy_third_party.js
@@ -20,7 +20,9 @@ module.exports = function(context) {
   console.log('Copying Android third party libraries...');
   child_process.execSync('mkdir -p plugins/cordova-plugin-outline/android/libs');
   child_process.execSync(
-      `cp third_party/outline-go-tun2socks/android/tun2socks.aar plugins/cordova-plugin-outline/android/libs/`);
+    `cp third_party/outline-go-tun2socks/android/tun2socks.aar plugins/cordova-plugin-outline/android/libs/`
+  );
   child_process.execSync(
-      `cp -R third_party/outline-go-tun2socks/android/jni plugins/cordova-plugin-outline/android/libs/obj`);
-}
+    `cp -R third_party/outline-go-tun2socks/android/jni plugins/cordova-plugin-outline/android/libs/obj`
+  );
+};
