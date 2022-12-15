48e38921c8759f6967998fdaa7eb0a7cb37c24e2
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2022-12-15 15:04:45 -0500

    update copydir

--- src/electron/build.action.mjs
+++ src/electron/build.action.mjs
@@ -36,7 +36,7 @@ export async function main(...parameters) {
   await runAction('electron/build_main', ...parameters);
 
   await copydir.sync(
-    path.join(getRootDir(), 'src/electron/icons'),
+    path.join(getRootDir(), `src/electron/${platform}/resources`),
     path.join(getRootDir(), ELECTRON_BUILD_DIR, 'icons')
   );
 
