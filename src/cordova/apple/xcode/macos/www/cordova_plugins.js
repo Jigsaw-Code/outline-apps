cbb283f5e43da6285172679a5f4882f8bcb4901e
Vinicius Fortuna <fortuna@users.noreply.github.com>
2022-12-15 16:31:33 -0500

    Cordova project diff
    This commit shows the edits we make to the standard Cordova project.
    We obtain the diff by reversing the rsync call.

--- src/cordova/apple/xcode/macos/www/cordova_plugins.js
+++ src/cordova/apple/xcode/macos/www/cordova_plugins.js
@@ -9,6 +9,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    },
+    {
       "id": "cordova-webintent.WebIntent",
       "file": "plugins/cordova-webintent/www/webintent.js",
       "pluginId": "cordova-webintent",
@@ -18,8 +26,10 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-outline": "0.0.0",
     "cordova-plugin-clipboard": "2.0.0",
+    "cordova-plugin-outline": "0.0.0",
+    "cordova-plugin-splashscreen": "6.0.2",
+    "cordova-plugin-statusbar": "2.4.3",
     "cordova-webintent": "2.0.0"
   };
 });
