ab342f6bd6236f3c6b05d9822e9d1840a3fb6a29
Vinicius Fortuna <fortuna@users.noreply.github.com>
2022-12-16 16:51:12 -0500

    Build xcframeworks in the build directory

--- src/cordova/plugin/apple/scripts/copy_third_party_frameworks_macos.js
+++ src/cordova/plugin/apple/scripts/copy_third_party_frameworks_macos.js
@@ -22,11 +22,11 @@ module.exports = function(context) {
   child_process.execSync('mkdir -p plugins/cordova-plugin-outline/apple/lib/macos');
 
   child_process.execSync('echo "Building CocoaLumberjack" && cd third_party/CocoaLumberjack && make');
-  child_process.execSync('cp -R third_party/CocoaLumberjack/Carthage/Build/CocoaLumberjack.xcframework plugins/cordova-plugin-outline/apple/lib/macos');
-  child_process.execSync('cp -R third_party/CocoaLumberjack/Carthage/Build/CocoaLumberjackSwift.xcframework plugins/cordova-plugin-outline/apple/lib/macos');
+  child_process.execSync('cp -R build/third_party/CocoaLumberjack/Carthage/Build/CocoaLumberjack.xcframework plugins/cordova-plugin-outline/apple/lib/macos');
+  child_process.execSync('cp -R build/third_party/CocoaLumberjack/Carthage/Build/CocoaLumberjackSwift.xcframework plugins/cordova-plugin-outline/apple/lib/macos');
 
   child_process.execSync('echo "Building sentry-cocoa" && cd third_party/sentry-cocoa && make');
-  child_process.execSync('cp -R third_party/sentry-cocoa/Carthage/Build/Sentry.xcframework plugins/cordova-plugin-outline/apple/lib/macos/');
+  child_process.execSync('cp -R build/third_party/sentry-cocoa/Carthage/Build/Sentry.xcframework plugins/cordova-plugin-outline/apple/lib/macos/');
 
   child_process.execSync(
       'cp -R third_party/outline-go-tun2socks/apple/Tun2socks.xcframework plugins/cordova-plugin-outline/apple/lib/macos/');
