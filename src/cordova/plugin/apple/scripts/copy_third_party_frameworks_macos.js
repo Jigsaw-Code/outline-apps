4bb696f2f5604832b2e570f34484ed47384335dc
Vinicius Fortuna <fortuna@users.noreply.github.com>
2022-12-27 13:35:50 -0500

    Use SPM for sentry-cocoa on macOS

--- src/cordova/plugin/apple/scripts/copy_third_party_frameworks_macos.js
+++ src/cordova/plugin/apple/scripts/copy_third_party_frameworks_macos.js
@@ -25,9 +25,6 @@ module.exports = function(context) {
   child_process.execSync('cp -R third_party/CocoaLumberjack/Carthage/Build/CocoaLumberjack.xcframework plugins/cordova-plugin-outline/apple/lib/macos', {stdio: 'inherit'});
   child_process.execSync('cp -R third_party/CocoaLumberjack/Carthage/Build/CocoaLumberjackSwift.xcframework plugins/cordova-plugin-outline/apple/lib/macos', {stdio: 'inherit'});
 
-  child_process.execSync('echo "Building sentry-cocoa" && cd third_party/sentry-cocoa && make', {stdio: 'inherit'});
-  child_process.execSync('cp -R third_party/sentry-cocoa/Carthage/Build/Sentry.xcframework plugins/cordova-plugin-outline/apple/lib/macos/', {stdio: 'inherit'});
-
   child_process.execSync(
       'cp -R third_party/outline-go-tun2socks/apple/Tun2socks.xcframework plugins/cordova-plugin-outline/apple/lib/macos/', {stdio: 'inherit'});
 }
