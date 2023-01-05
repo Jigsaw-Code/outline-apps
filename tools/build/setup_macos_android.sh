6123c0f4e08932d8439995abd29af36009d4a2e1
Vinicius Fortuna <fortuna@users.noreply.github.com>
2023-01-05 10:32:18 -0500

    Fix quotes

--- tools/build/setup_macos_android.sh
+++ tools/build/setup_macos_android.sh
@@ -20,19 +20,19 @@ set -eu
 #   https://developer.android.com/studio/releases/build-tools.html
 # To find the latest version's label:
 #   sdkmanager --list|grep build-tools
-ANDROID_BUILD_TOOLS_VERSION=${ANDROID_BUILD_TOOLS_VERSION:-"30.0.3"}
+ANDROID_BUILD_TOOLS_VERSION=${ANDROID_BUILD_TOOLS_VERSION:-'30.0.3'}
 
 # NDK (side by side) version must be kept in sync with the default build tools NDK version.
-NDK_VERSION=${NDK_VERSION:-"21.0.6113669"}
+NDK_VERSION=${NDK_VERSION:-'21.0.6113669'}
 
 function install_jdk() {
   # Cordova Android 10 has to use JDK 11.
   if [[ -d "$HOME/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk" ]]; then
-    echo "JDK already installed"
+    echo 'JDK already installed'
     return
   fi
 
-  echo "Downloading JDK"
+  echo 'Downloading JDK'
   curl https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_osx-x64_bin.tar.gz | tar -xzk -C "$HOME/Library/Java/JavaVirtualMachines/"
 }
 
@@ -51,17 +51,17 @@ function install_android_tools() {
     rm -r "${tmp_zip_dir}"
   fi
 
-  echo "Installing build-tools and ndk"
+  echo 'Installing build-tools and ndk'
   "${cmdline_tools_dir}/bin/sdkmanager" "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" "ndk;${NDK_VERSION}"
 }
 
 function install_gradle() {
-  if which gradle > /dev/null; then
-    echo "Gradle already installed"
+  if which -s gradle; then
+    echo 'Gradle already installed'
   else
-    if ! which brew > /dev/null; then
+    if ! which -s brew; then
       # TODO(fortuna): Install gradle without Homebrew: https://gradle.org/install/#manually
-      echo "Homebrew is not installed. You need to manually install it: https://docs.brew.sh/Installation" > 2
+      echo 'Homebrew is not installed. You need to manually install it: https://docs.brew.sh/Installation' > 2
       exit 1
     fi
     brew install gradle
@@ -73,7 +73,7 @@ function main() {
   # For Cordova Android requirements.
 
   if [[ "$(uname -s)" != 'Darwin' ]]; then
-    echo "Must run from a macOS machine" > 2
+    echo 'Must run from a macOS machine' > 2
     exit 1
   fi
   
@@ -87,7 +87,7 @@ function main() {
   install_gradle
   gradle --version
 
-  echo "Setup done. Make sure to define these environment variables:"
+  echo 'Setup done. Make sure to define these environment variables:'
   echo "export ANDROID_SDK_ROOT=${android_home}"
   echo 'export PATH="$PATH:${ANDROID_SDK_ROOT}/platform-tools:${ANDROID_SDK_ROOT}/cmdline-tools/8.0/bin:${ANDROID_SDK_ROOT}/emulator"'
 }
