cb3ccb18a002e13a4968f457cd267353b481f278
Paul Joey Clark <joeytwiddle@gmail.com>
2023-01-07 16:27:23 +0800

    chore(build) Better error reporting: report exit code, and convert any type of error object into String

--- src/build/run_action.mjs
+++ src/build/run_action.mjs
@@ -93,9 +93,10 @@ export async function runAction(actionPath, ...parameters) {
   try {
     await spawnStream(runner, [...subCommands, resolvedPath, ...parameters]);
   } catch (error) {
-    if (error?.message) {
-      console.error(chalk.red(error.message));
+    if (error?.exitCode) {
+      console.error(chalk.red(`Exit code: ${error.exitCode} ${error.constructor?.name}`));
     }
+    console.error(chalk.red(String(error)));
     console.groupEnd();
     console.error(chalk.red.bold(`▶ action(${actionPath}):`), chalk.red(`❌ Failed.`));
 
