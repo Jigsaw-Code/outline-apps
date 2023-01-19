c29197957a6cb2ea84ba4d619d97f4d836f98094
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2023-01-19 17:27:16 -0500

    Revert "fix: temporarily remove client feedback view (#1548)" (#1559)
    This reverts commit 539c0026a946e99b772a5905784f7977c2b32ae5.

--- src/www/ui_components/app-root.js
+++ src/www/ui_components/app-root.js
@@ -343,11 +343,10 @@ export class AppRoot extends mixinBehaviors([AppLocalizeBehavior], PolymerElemen
               <iron-icon icon="outline-icons:outline" slot="item-icon"></iron-icon>
               <span class="item-label">[[localize('servers-menu-item')]]</span>
             </paper-icon-item>
-            <!-- TODO(daniellacosse): fix sentry and restore feedback view -->
-            <!-- <paper-icon-item name="feedback">
+            <paper-icon-item name="feedback">
               <iron-icon id="feedback-icon" icon="feedback" slot="item-icon"></iron-icon>
               [[localize('feedback-page-title')]]
-            </paper-icon-item> -->
+            </paper-icon-item>
             <paper-icon-item name="about">
               <iron-icon icon="info" slot="item-icon"></iron-icon>
               [[localize('about-page-title')]]
