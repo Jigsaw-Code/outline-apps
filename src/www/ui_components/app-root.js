2faace84f314cb7d9a473d2f19489a8e1abbbe09
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2023-02-28 17:19:48 -0500

    fix: use hidden directly on paper-item

--- src/www/ui_components/app-root.js
+++ src/www/ui_components/app-root.js
@@ -362,12 +362,10 @@ export class AppRoot extends mixinBehaviors([AppLocalizeBehavior], PolymerElemen
               <img src$="[[rootPath]]assets/icons/outline.png" alt="outline"  />
               <span class="item-label">[[localize('servers-menu-item')]]</span>
             </paper-item>
-            <span hidden$="[[shouldHideFeedback]]">
-              <paper-item name="feedback">
-                <img src$="[[rootPath]]assets/icons/feedback.png" alt="feedback"  />
-                [[localize('feedback-page-title')]]
-              </paper-item>
-            </span>
+            <paper-item name="feedback" hidden$="[[shouldHideFeedback]]">
+              <img src$="[[rootPath]]assets/icons/feedback.png" alt="feedback"  />
+              [[localize('feedback-page-title')]]
+            </paper-item>
             <paper-item name="about">
               <img src$="[[rootPath]]assets/icons/about.png" alt="about"  />
               [[localize('about-page-title')]]
