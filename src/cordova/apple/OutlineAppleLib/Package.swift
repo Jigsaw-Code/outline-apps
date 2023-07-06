b46650bdc96f7f51c8e9adb99504d7f3a74cb2df
Vinicius Fortuna <fortuna@users.noreply.github.com>
2023-07-06 17:48:12 -0400

    Update dependencies

--- src/cordova/apple/OutlineAppleLib/Package.swift
+++ src/cordova/apple/OutlineAppleLib/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Tun2socks", "OutlineTunnel", "PacketTunnelProvider"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", exact: "3.7.4"),
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.4"),
     ],
     targets: [
         .target(
