47ae8149e7becdbdc65cacb25712e81e3d894e26
Sander Bruens <sbruens@users.noreply.github.com>
2023-07-24 11:52:03 -0400

    Move `AppDelegate` logic to category extension instead of overwriting the `AppDelegate` file auto-generated by Cordova.

---
+++ src/cordova/apple/xcode/ios/Outline/Classes/AppDelegate+Outline.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate+Outline.h
+//  Outline
+//
+//  Created by Sander Bruens on 7/24/23.
+//
+
+#ifndef AppDelegate_Outline_h
+#define AppDelegate_Outline_h
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (Outline) <UIApplicationDelegate>
+@end
+
+#endif /* AppDelegate_Outline_h */
+
