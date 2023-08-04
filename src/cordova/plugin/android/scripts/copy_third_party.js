1c2e8c2b468824d3905dcef1b70a329dfa3b835a
jyyi1 <yijunyi@google.com>
2023-08-04 16:54:06 -0400

    use path.join to make paths more platform agnostic

--- src/cordova/plugin/android/scripts/copy_third_party.js
+++ src/cordova/plugin/android/scripts/copy_third_party.js
@@ -17,8 +17,8 @@
 const fs = require('fs/promises');
 const path = require('node:path');
 
-const ANDROID_LIBS_FOLDER_PATH = 'plugins/cordova-plugin-outline/android/libs';
-const TUN2SOCKS_ANDROID_FOLDER_PATH = 'third_party/outline-go-tun2socks/android';
+const ANDROID_LIBS_FOLDER_PATH = path.join('plugins', 'cordova-plugin-outline', 'android', 'libs');
+const TUN2SOCKS_ANDROID_FOLDER_PATH = path.join('third_party', 'outline-go-tun2socks', 'android');
 
 module.exports = async function(context) {
   console.log('Copying Android third party libraries...');
