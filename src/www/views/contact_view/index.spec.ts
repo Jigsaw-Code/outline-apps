0ce368a0f289c0bb3132cb593a9b9b22d31ce134
Sander Bruens <sbruens@users.noreply.github.com>
2023-09-15 17:10:09 -0400

    Add initial test of contact view.

---
+++ src/www/views/contact_view/index.spec.ts
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2023 The Outline Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ContactView} from './index';
+
+import {fixture, html} from '@open-wc/testing';
+
+describe('ContactView', () => {
+  it('is defined', async () => {
+    const element = await fixture(
+      html`
+        <contact-view></contact-view>
+      `
+    );
+    expect(element).toBeInstanceOf(ContactView);
+  });
+
+  it('hides issue selector by default', async () => {
+    const element = await fixture(
+      html`
+        <contact-view></contact-view>
+      `
+    );
+    const issueSelector = element.shadowRoot.querySelector('mwc-select[label="Outline issue"]');
+    expect(issueSelector.hasAttribute('hidden')).toBeTrue();
+  });
+});
