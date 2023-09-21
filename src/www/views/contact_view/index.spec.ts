27a942b615cfb493c49f2baddcf59f40b31b4aad
Sander Bruens <sbruens@users.noreply.github.com>
2023-09-20 20:25:04 -0400

    Add more `ContactView` test cases.

--- src/www/views/contact_view/index.spec.ts
+++ src/www/views/contact_view/index.spec.ts
@@ -16,25 +16,109 @@
 
 import {ContactView} from './index';
 
-import {fixture, html} from '@open-wc/testing';
+import {fixture, html, nextFrame} from '@open-wc/testing';
 
 describe('ContactView', () => {
-  it('is defined', async () => {
-    const element = await fixture(
+  let el: ContactView;
+
+  beforeEach(async () => {
+    el = await fixture(
       html`
         <contact-view></contact-view>
       `
     );
-    expect(element).toBeInstanceOf(ContactView);
+  });
+
+  it('is defined', async () => {
+    expect(el).toBeInstanceOf(ContactView);
   });
 
   it('hides issue selector by default', async () => {
-    const element = await fixture(
-      html`
-        <contact-view></contact-view>
-      `
-    );
-    const issueSelector = element.shadowRoot?.querySelector('mwc-select[label="Outline issue"]');
+    const issueSelector = el.shadowRoot?.querySelector('mwc-select[label="Outline issue"]');
     expect(issueSelector?.hasAttribute('hidden')).toBeTrue();
+  });
+
+  it('hides support form by default', async () => {
+    const supportForm = el.shadowRoot?.querySelector('support-form');
+    expect(supportForm).toBeNull();
+  });
+
+  it('shows exit message if the user selects that they have an open ticket', async () => {
+    const radioButton = el.shadowRoot!.querySelector('mwc-formfield[label="Yes"] mwc-radio')! as HTMLElement;
+    radioButton.click();
+    await nextFrame();
+
+    const exitCard = el.shadowRoot!.querySelector('outline-card')!;
+    expect(exitCard.textContent).toContain('experiencing high support volume');
+  });
+
+  it('shows issue selector if the user selects that they have no open tickets', async () => {
+    const radioButton = el.shadowRoot!.querySelector('mwc-formfield[label="No"] mwc-radio')! as HTMLElement;
+    radioButton.click();
+    await nextFrame();
+
+    const issueSelector = el.shadowRoot!.querySelector('mwc-select[label="Outline issue"]');
+    expect(issueSelector?.hasAttribute('hidden')).toBeFalse();
+  });
+
+  describe('when the user selects issue', () => {
+    let issueSelector: HTMLElement;
+
+    beforeEach(async () => {
+      issueSelector = el.shadowRoot!.querySelector('mwc-select[label="Outline issue"]')!;
+      const radioButton: HTMLElement = el.shadowRoot!.querySelector('mwc-formfield[label="No"] mwc-radio')!;
+      radioButton.click();
+      await nextFrame();
+    });
+
+    it('"I need an access key" shows exit message', async () => {
+      const issue: HTMLElement = issueSelector.querySelector('mwc-list-item[value="require_access_key"]')!;
+      issue.click();
+      await nextFrame();
+
+      const exitCard = el.shadowRoot!.querySelector('outline-card')!;
+      expect(exitCard.textContent).toContain('does not distribute free or paid access keys');
+    });
+
+    it('"I am having trouble adding a server using my access key" shows exit message', async () => {
+      const issue: HTMLElement = issueSelector.querySelector('mwc-list-item[value="adding_server"]')!;
+      issue.click();
+      await nextFrame();
+
+      const exitCard = el.shadowRoot!.querySelector('outline-card')!;
+      expect(exitCard.textContent).toContain('assist with adding a server');
+    });
+
+    it('"I am having trouble connecting to my Outline VPN server" shows exit message', async () => {
+      const issue: HTMLElement = issueSelector.querySelector('mwc-list-item[value="connecting"]')!;
+      issue.click();
+      await nextFrame();
+
+      const exitCard = el.shadowRoot!.querySelector('outline-card')!;
+      expect(exitCard.textContent).toContain('assist with connecting to a server');
+    });
+
+    describe('"General feedback & suggestions"', () => {
+      beforeEach(async () => {
+        const issue: HTMLElement = issueSelector.querySelector('mwc-list-item[value="general"]')!;
+        issue.click();
+        await nextFrame();
+      });
+
+      it('shows support form', async () => {
+        const supportForm = el.shadowRoot!.querySelector('support-form')!;
+        expect(supportForm).not.toBeNull();
+      });
+
+      it('shows "thank you" exit message on completion of support form', async () => {
+        const supportForm = el.shadowRoot!.querySelector('support-form')!;
+        supportForm.dispatchEvent(new CustomEvent('submit'));
+
+        await nextFrame();
+
+        const exitCard = el.shadowRoot!.querySelector('outline-card')!;
+        expect(exitCard.textContent).toContain('Thanks for helping us improve');
+      });
+    });
   });
 });
