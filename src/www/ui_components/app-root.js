cf6747e56690e720208da67814c6eb12a66a826c
Sander Bruens <sbruens@users.noreply.github.com>
2023-09-29 22:23:34 -0400

    Disable `contactViewFeatureFlag`.

--- src/www/ui_components/app-root.js
+++ src/www/ui_components/app-root.js
@@ -606,7 +606,7 @@ export class AppRoot extends mixinBehaviors([AppLocalizeBehavior], PolymerElemen
       contactViewFeatureFlag: {
         type: Boolean,
         readonly: true,
-        value: true,
+        value: false,
       },
     };
   }
