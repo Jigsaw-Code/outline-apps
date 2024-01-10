fc64b8272993e98f1cd62bf6c72be14dd00a9933
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-01-10 16:55:43 -0500

    attempt #1

--- src/cordova/plugin/android/scripts/copy_third_party.js
+++ src/cordova/plugin/android/scripts/copy_third_party.js
@@ -18,7 +18,7 @@ const fs = require('fs/promises');
 const path = require('node:path');
 
 const ANDROID_LIBS_FOLDER_PATH = path.join('plugins', 'cordova-plugin-outline', 'android', 'libs');
-const TUN2SOCKS_ANDROID_FOLDER_PATH = path.join('third_party', 'outline-go-tun2socks', 'android');
+const TUN2SOCKS_ANDROID_FOLDER_PATH = path.join('output', 'build', 'android');
 
 module.exports = async function (context) {
   console.log('Copying Android third party libraries...');
