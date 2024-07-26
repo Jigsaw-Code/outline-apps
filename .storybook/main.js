9633331336668a56934baa050798301f1b1ffab9
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-07-26 11:01:30 -0400

    Update .storybook/main.js
    Co-authored-by: Sander Bruens <sbruens@users.noreply.github.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 import {join, dirname} from 'path';
 
 /**
- * This function is used to resolve the absolute path of a package.
+ * Resolves the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
