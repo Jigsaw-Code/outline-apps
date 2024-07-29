67a91359f42430b8492264eef94f03e770e9e347
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-07-29 13:17:33 -0400

    add quotes

--- server_manager/www/server_install.spec.ts
+++ server_manager/www/server_install.spec.ts
@@ -28,7 +28,7 @@ describe('getShellExportCommands', () => {
         "export WATCHTOWER_REFRESH_SECONDS='999'\n" +
         "export SENTRY_API_URL='https://sentry.example/'\n" +
         "export SB_METRICS_URL='https://metrics.example/'\n" +
-        "export SB_METRICS_ENABLED=false\n" +
+        "export SB_METRICS_ENABLED='false'\n" +
         'export SB_DEFAULT_SERVER_NAME="$(printf \'Outline Server Foo\')"\n'
     );
   });
@@ -39,7 +39,7 @@ describe('getShellExportCommands', () => {
       metricsUrl: '',
     };
     const serverName = '';
-    expect(getShellExportCommands(settings, serverName, false)).toEqual('"export SB_METRICS_ENABLED=false\nexport SB_DEFAULT_SERVER_NAME="$(printf \'\')"\n');
+    expect(getShellExportCommands(settings, serverName, false)).toEqual('"export SB_METRICS_ENABLED=\'false\'\nexport SB_DEFAULT_SERVER_NAME="$(printf \'\')"\n');
   });
 
   it('server name escaping', () => {
@@ -49,7 +49,7 @@ describe('getShellExportCommands', () => {
     };
     const serverName = 'Outline Server فرانكفورت';
     expect(getShellExportCommands(settings, serverName, false)).toEqual(
-      '"export SB_METRICS_ENABLED=false\nexport SB_DEFAULT_SERVER_NAME="$(printf \'Outline Server \\u0641\\u0631\\u0627\\u0646\\u0643\\u0641\\u0648\\u0631\\u062a\')"\n'
+      '"export SB_METRICS_ENABLED=\'false\'\nexport SB_DEFAULT_SERVER_NAME="$(printf \'Outline Server \\u0641\\u0631\\u0627\\u0646\\u0643\\u0641\\u0648\\u0631\\u062a\')"\n'
     );
   });
 });
