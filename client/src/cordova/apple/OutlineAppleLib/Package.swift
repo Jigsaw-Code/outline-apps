b054e53d96b30f509c032e9eb5f115cc000025c0
Sander Bruens <sbruens@users.noreply.github.com>
2024-08-02 11:12:18 -0400

    Fix build and add addlicense to Taskfile.

--- client/src/cordova/apple/OutlineAppleLib/Package.swift
+++ client/src/cordova/apple/OutlineAppleLib/Package.swift
@@ -1,3 +1,6 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 // Copyright 2024 The Outline Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,9 +14,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
