b1fdde370c223b49128032f6b05d7251fe88fe00
Vinicius Fortuna <fortuna@users.noreply.github.com>
2024-08-05 16:38:06 -0400

    Fix .js

--- client/src/www/app/main.ts
+++ client/src/www/app/main.ts
@@ -20,7 +20,7 @@ import {makeConfig, SIP002_URI} from 'ShadowsocksConfig';
 import {App} from './app';
 import {onceEnvVars} from './environment';
 import {OutlineServerRepository} from './outline_server_repository';
-import {FAKE_BROKEN_HOSTNAME, FAKE_UNREACHABLE_HOSTNAME, FakeVpnApi} from './outline_server_repository/vpn.fake.js';
+import {FAKE_BROKEN_HOSTNAME, FAKE_UNREACHABLE_HOSTNAME, FakeVpnApi} from './outline_server_repository/vpn.fake';
 import {OutlinePlatform} from './platform';
 import {Settings} from './settings';
 import {EventQueue} from '../model/events';
