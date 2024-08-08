41177cc70db66bfd3516de3017794f1b2dbf8db5
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-08-08 16:06:19 -0400

    feat(client): reload UI on www source change

---
+++ client/src/cordova/start.action.mjs
@@ -0,0 +1,87 @@
+// Copyright 2022 The Outline Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import path from 'path';
+import url from 'url';
+
+import {createReloadServer} from '@outline/infrastructure/build/create_reload_server.mjs';
+import {getRootDir} from '@outline/infrastructure/build/get_root_dir.mjs';
+import {runAction} from '@outline/infrastructure/build/run_action.mjs';
+import cordovaLib from 'cordova-lib';
+import {hashElement} from 'folder-hash';
+import * as fs from "fs-extra";
+const {cordova} = cordovaLib;
+
+import {getBuildParameters} from '../../build/get_build_parameters.mjs';
+
+const getUIHash = async () => {
+  const hashResult = await hashElement(
+    path.join(getRootDir(), 'client/src/www'),
+    {
+      files: {include: ['**/*.ts', '**/*.html', '**/*.css', '**/*.js']},
+    }
+  );
+
+  return hashResult.hash;
+};
+
+/**
+ * @description Builds the parameterized cordova binary (ios, macos, maccatalyst, android).
+ *
+ * @param {string[]} parameters
+ */
+export async function main(...parameters) {
+  const {platform, verbose} = getBuildParameters(parameters);
+
+  if (platform !== 'macos') {
+    throw new Error('Only macos platform is currently supported');
+  }
+
+  await runAction('client/src/www/build', ...parameters);
+  await runAction('client/go/build', ...parameters);
+  await runAction('client/src/cordova/setup', ...parameters);
+
+  if (verbose) {
+    cordova.on('verbose', message =>
+      console.debug(`[cordova:verbose] ${message}`)
+    );
+  }
+
+  let previousUIHashResult = await getUIHash();
+
+  console.log('Starting reload server @ port 35729...');
+  createReloadServer(async () => {
+    const currentUIHashResult = await getUIHash();
+
+    if (previousUIHashResult === currentUIHashResult) {
+      return false;
+    }
+
+    previousUIHashResult = currentUIHashResult;
+
+    await runAction('client/src/www/build', ...parameters);
+
+    console.log('Copying www folder to platforms/osx/www...');
+    fs.copySync(
+      path.join(getRootDir(), 'client/www'),
+      path.join(getRootDir(), 'client/platforms/osx/www')
+    );
+
+    return true;
+  }).listen(35729);
+}
+
+if (import.meta.url === url.pathToFileURL(process.argv[1]).href) {
+  await main(...process.argv.slice(2));
+}
