3556de2eec6fdf0668baf930ecf80f0e3e40a256
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-08-15 10:11:00 -0400

    ok that seems closer

---
+++ client/generated/messages/input/ms.ts
@@ -0,0 +1,25 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    import {html} from 'lit';
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      'h4d2b09d19c4029bc': html`Version ${0} (${1})`,
+'h3cbf51139d0ac4af': html`Outline is a product by
+              <a href="https://jigsaw.google.com">Jigsaw</a> that lets anyone
+              create, run, and share access to their own VPN. Outline is
+              designed to be resistant to blocking. Visit
+              <a href="https://getoutline.org">getoutline.org</a> to learn how
+              to get started.<br><br>Outline is open source software powered
+              by <a href="https://shadowsocks.org">Shadowsocks</a>. You can
+              contribute to the code on
+              <a href="https://github.com/jigsaw-Code/?q=outline">GitHub</a> and
+              join the conversation on
+              <a href="https://www.reddit.com/r/outlinevpn">Reddit</a>.`,
+    };
+  
