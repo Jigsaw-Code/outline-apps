38e51e8fc193f009f7d4417254740b8c18e0ace8
jyyi1 <yijunyi@google.com>
2024-10-04 15:32:28 -0400

    Add comments and unit test

--- client/electron/go_vpn_tunnel.ts
+++ client/electron/go_vpn_tunnel.ts
@@ -68,6 +68,8 @@ export class GoVpnTunnel implements VpnTunnel {
     private readonly routing: RoutingDaemon,
     readonly transportConfig: TransportConfigJson
   ) {
+    // We need two GoTun2socks instances because we need to update the UDP connectivity
+    // using this.connectivityChecker while this.tun2socks is still running.
     this.tun2socks = new GoTun2socks();
 
     // This promise, tied to both helper process' exits, is key to the instance's
