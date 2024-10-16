627562de56f7dcdc6934e1484cce7491f199d164
J. Yi <93548144+jyyi1@users.noreply.github.com>
2024-10-16 13:29:55 -0400

    fix linting error

--- client/src/www/app/resource_fetcher.electron.ts
+++ client/src/www/app/resource_fetcher.electron.ts
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {PlatformError} from '../model/platform_error';
 import {ResourceFetcher} from './resource_fetcher';
+import {PlatformError} from '../model/platform_error';
 
 /**
  * Fetches resources using Electron's IPC to communicate with the main process.
