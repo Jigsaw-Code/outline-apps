372f177cfb695f81b30a567214f77e095864ad90
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2025-04-15 16:13:12 -0400

    revert cidranger

--- client/go/outline/routingtable/ip_routing_table.go
+++ client/go/outline/routingtable/ip_routing_table.go
@@ -15,12 +15,9 @@
 package routingtable
 
 import (
-	"errors"
-	"net"
+	"container/list"
+	"fmt"
 	"net/netip"
-
-	"github.com/yl2chen/cidranger"
-	"go4.org/netipx"
 )
 
 // Compile-time check
@@ -28,41 +25,52 @@ var _ RoutingTable[netip.Prefix, netip.Addr, any] = (*IPRoutingTable[any])(nil)
 
 // "V" is typically expected to be a dialer of some kind
 type IPRoutingTable[V any] struct {
-	prefixTrie cidranger.Ranger
-}
-
-type IPRoutingTableEntry[V any] struct {
-	cidranger.RangerEntry
-	Value V
+	prefixBuckets map[int]map[netip.Prefix]V
+	prefixLengths *list.List
 }
 
 func NewIPRoutingTable[V any]() *IPRoutingTable[V] {
 	return &IPRoutingTable[V]{
-		prefixTrie: cidranger.NewPCTrieRanger(),
+		prefixBuckets: make(map[int]map[netip.Prefix]V),
+		prefixLengths: list.New(),
 	}
 }
 
 func (table *IPRoutingTable[V]) AddRoute(prefixRule netip.Prefix, dialer V) error {
-	ipNet := netipx.PrefixIPNet(prefixRule)
+	length := prefixRule.Bits()
+	if _, ok := table.prefixBuckets[length]; !ok {
+		table.prefixBuckets[length] = make(map[netip.Prefix]V)
 
-	table.prefixTrie.Insert(&IPRoutingTableEntry[V]{
-		RangerEntry: cidranger.NewBasicRangerEntry(*ipNet),
-		Value:       dialer,
-	})
+		inserted := false
+
+		for node := table.prefixLengths.Front(); node != nil; node = node.Next() {
+			currentLength := node.Value.(int)
+			if length > currentLength {
+				table.prefixLengths.InsertBefore(length, node)
+				inserted = true
+				break
+			}
+		}
+
+		if !inserted {
+			table.prefixLengths.PushBack(length)
+		}
+	}
+
+	table.prefixBuckets[length][prefixRule] = dialer
 	return nil
 }
 
-func (table *IPRoutingTable[V]) Lookup(matchKey netip.Addr) (V, error) {
-	entry, err := table.prefixTrie.ContainingNetworks(net.ParseIP(matchKey.String()))
-
-	var zeroValue V
-	if err != nil {
-		return zeroValue, err
+func (table *IPRoutingTable[V]) Lookup(lookupAddress netip.Addr) (V, error) {
+	for node := table.prefixLengths.Front(); node != nil; node = node.Next() {
+		bucket := table.prefixBuckets[node.Value.(int)]
+		for route, dialer := range bucket {
+			if route.Contains(lookupAddress) {
+				return dialer, nil
+			}
+		}
 	}
 
-	if len(entry) == 0 {
-		return zeroValue, errors.New("no entries found")
-	}
-
-	return entry[len(entry)-1].(*IPRoutingTableEntry[V]).Value, nil
+	var zeroV V
+	return zeroV, fmt.Errorf("no matching prefix found for %s", lookupAddress.String())
 }
