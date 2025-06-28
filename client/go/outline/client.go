690a0d81c783d69e79ade50560b7f1315596a8cb
nomadali <68923563+62w71st@users.noreply.github.com>
2025-06-28 22:32:00 +0000

    Addressed minor comment.

--- client/go/outline/client.go
+++ client/go/outline/client.go
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,7 +17,6 @@ package outline
 import (
 	"context"
 	"errors"
-	"fmt"
 	"net"
 
 	"github.com/Jigsaw-Code/outline-apps/client/go/configyaml"
@@ -154,7 +153,6 @@ func NewClientWithBaseDialers(clientConfigText string, sessionConfig string, tcp
 			}
 		}
 	}
-	fmt.Println("usageReporter", usageReporter)
 
 	return &Client{sd: transportPair.StreamDialer, pl: transportPair.PacketListener, ur: usageReporter}, nil
 }
