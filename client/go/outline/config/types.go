d329b97a1832b725fe940c05db1eb32abe7c4a80
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2025-08-26 14:09:42 -0400

    feat(client): iptable dialer outline config support (#2493)
    * feat(client): ip table outline config support
    
    * eh, assume tunneled, it's fine.
    
    * packet listener progress
    
    * wip
    
    * wip
    
    * something like this
    
    * okay, let's ask vini now
    
    * hoist thread lock, guess go maps aren't threadsafe
    
    * in-flight feedback
    
    * draft listener parser
    
    * wip test
    
    * unit test works
    
    * update config references
    
    * full parsing test
    
    * Update client/go/outline/config/config_shadowsocks.go
    
    Co-authored-by: J. Yi <93548144+jyyi1@users.noreply.github.com>
    
    * Update client/go/outline/config/module.go
    
    Co-authored-by: J. Yi <93548144+jyyi1@users.noreply.github.com>
    
    * feedback
    
    * Don't need to expect panic anymore
    
    * import fixes
    
    * remove ip-table dash elsewhere
    
    * Update client/go/outline/config/config_iptable.go
    
    Co-authored-by: J. Yi <93548144+jyyi1@users.noreply.github.com>
    
    * Update client/go/outline/config/module.go
    
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>
    
    * Update client/go/outline/config/config_iptable_test.go
    
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>
    
    * Update client/go/outline/config/module_test.go
    
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>
    
    * remove logs
    
    * additional feedback
    
    * move subparser out of root module
    
    * detect partial tunneling
    
    * streamline go code
    
    * update tests
    
    * feedback
    
    * Update client/go/outline/config/config_iptable.go
    
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>
    
    * Update client/go/outline/config/config_iptable.go
    
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>
    
    * Add fallback option
    
    * add ips field, remove unneeded bool
    
    * missed a spot
    
    * oops
    
    * oops #2
    
    * Hoist ipsDialer creation, correctly pass nil fallback
    
    * Remove unnecessary nil assignment.
    
    * Fixes "null -> nil" issue on fallback by checking for key existence
    
    * null -> direct unit test
    
    * feedback + tests
    
    * remove null dialers from unit test
    
    ---------
    
    Co-authored-by: J. Yi <93548144+jyyi1@users.noreply.github.com>
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>

--- client/go/outline/config/types.go
+++ client/go/outline/config/types.go
@@ -28,6 +28,7 @@ type ConnType int
 const (
 	ConnTypeDirect ConnType = iota
 	ConnTypeTunneled
+	ConnTypePartial
 )
 
 // ConnProviderConfig represents a dialer or endpoint that can create connections.
---
+++ client/go/outline/iptable/stream_dialer.go
@@ -0,0 +1,80 @@
+// Copyright 2025 The Outline Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package iptable
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/netip"
+
+	"github.com/Jigsaw-Code/outline-sdk/transport"
+)
+
+func lookupInTable[D any](table IPTable[D], address string) D {
+	host := address
+	if _host, _, err := net.SplitHostPort(address); err == nil {
+		host = _host
+	}
+
+	ip, err := netip.ParseAddr(host)
+	if err == nil {
+		return table.Lookup(ip)
+	}
+
+	var zeroD D
+	return zeroD
+}
+
+// StreamDialer is a [transport.StreamDialer] that routes connections
+// based on the destination IP address using an [IPTable].
+// If a specific route is found in the table, the corresponding dialer is used.
+// Otherwise, the default dialer (if set) is used.
+type StreamDialer struct {
+	table    IPTable[transport.StreamDialer]
+	fallback transport.StreamDialer
+}
+
+// NewStreamDialer creates a new [StreamDialer].
+// If the provided table is nil, a new empty table will be created internally.
+// It returns the new dialer and a nil error.
+func NewStreamDialer(table IPTable[transport.StreamDialer], fallback transport.StreamDialer) (*StreamDialer, error) {
+	if table == nil {
+		table = NewIPTable[transport.StreamDialer]()
+	}
+	return &StreamDialer{
+		table:    table,
+		fallback: fallback,
+	}, nil
+}
+
+// DialStream dials the given address using the appropriate [transport.StreamDialer]
+// determined by looking up the destination IP in the IP table.
+// If no specific route is found, it uses the fallback dialer.
+// If no specific route is found and no fallback dialer is set, or if the
+// selected dialer fails, it returns an error.
+func (dialer *StreamDialer) DialStream(ctx context.Context, address string) (transport.StreamConn, error) {
+	selectedDialer := lookupInTable(dialer.table, address)
+
+	if selectedDialer == nil && dialer.fallback != nil {
+		return dialer.fallback.DialStream(ctx, address)
+	}
+
+	if selectedDialer == nil {
+		return nil, fmt.Errorf("no dialer available for address %s", address)
+	}
+
+	return selectedDialer.DialStream(ctx, address)
+}
