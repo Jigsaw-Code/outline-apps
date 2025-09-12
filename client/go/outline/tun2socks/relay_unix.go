3a1cd0f7dc66280b0eeb6e55c16a637d75605544
jyyi1 <yijunyi@google.com>
2025-09-12 16:15:41 -0400

    add build constraint to tun2socks/relay.go

--- client/go/outline/tun2socks/relay_unix.go
+++ client/go/outline/tun2socks/relay_unix.go
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build unix
+
+// to prevent "undefined: unix.Dup" error
+
 package tun2socks
 
 import (
