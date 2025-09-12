48c0a6287eaa25f6e74104509f048c53b70cdc16
jyyi1 <yijunyi@google.com>
2025-09-12 17:28:12 -0400

    fix capitalized error message warning

--- client/go/outline/tun2socks/relay_unix.go
+++ client/go/outline/tun2socks/relay_unix.go
@@ -69,7 +69,7 @@ func GoRelayTraffic(fd int, rd *RemoteDevice) *perrs.PlatformError {
 // separately closed.  (UNIX only.)
 func makeTunFile(fd int) (*os.File, error) {
 	if fd < 0 {
-		return nil, errors.New("Must provide a valid TUN file descriptor")
+		return nil, errors.New("must provide a valid TUN file descriptor")
 	}
 	// Make a copy of `fd` so that os.File's finalizer doesn't close `fd`.
 	newfd, err := unix.Dup(fd)
@@ -78,7 +78,7 @@ func makeTunFile(fd int) (*os.File, error) {
 	}
 	file := os.NewFile(uintptr(newfd), "")
 	if file == nil {
-		return nil, errors.New("Failed to open TUN file descriptor")
+		return nil, errors.New("failed to open TUN file descriptor")
 	}
 	return file, nil
 }
